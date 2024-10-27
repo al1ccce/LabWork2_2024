55cb5893bf846c10102571bd86462eaebf8b5218
al1ccce <96458271+al1ccce@users.noreply.github.com>
2024-10-27 16:22:58 +0700

    full project

--- funcs1.cpp
+++ funcs1.cpp
@@ -89,9 +89,111 @@ void task1(){
     cout << opers.get() << endl;
 }
 
-void task3(){
-    string templ = "*@stud.nstu.?";
-    cout << templ;
-    string da = "meow@stud.nstu.com"
+void task4(){
+    Vector<char> syms;
+    for (int i = 33; i <= 41; ++i) {
+        syms.push_back(static_cast<char>(i));
+    }
+    for (int i = 43; i <= 47; ++i) {
+        syms.push_back(static_cast<char>(i));
+    }
+    for (int i = 58; i <= 62; ++i) {
+        syms.push_back(static_cast<char>(i));
+    }
+    syms.push_back(static_cast<char>(64));
+    string templ = "*?stud.nstu.??";
+    cout << templ << endl;
+    string da = "meow@stud.nstu.co";
+    cout << da << endl;
+    int i = 0, j = 0;
+    while (i < da.size() && j < templ.size()) {
+        if (da[i] == templ[j] || templ[j] == '?') {
+            i++;
+            j++;
+        } else if (templ[j] == '*') {
+            while (i < da.size() && !syms.find(da[i])) {
+                i++;
+            }
+            j++;
+        } else {
+            cout << "Сломался на символе шаблона " << templ[j] << " и символе строки " << da[i] << endl;
+            cout << "No.\n";
+            return;
+        }
+    }
 
+    while (j < templ.size() && templ[j] == '*') {
+        j++;
+    }
+
+    if (i == da.size() && j == templ.size()) {
+        cout << "Yes.\n";
+    } else {
+        cout << "No.\n";
+    }
+}
+
+void task5(){
+    cout << "elements: ";
+    int el;
+    FullBinaryTree tree;
+    while(true){
+        cin >> el;
+        if (el == 0){
+            break;
+        }
+        tree.insert(tree.root, el);
+    }
+    tree.printbash();
+    cout << "delete: ";
+    cin >> el;
+    if (tree.search(el) == nullptr){
+        cout << "Элемент отсутствует в дереве" << endl;
+    }
+    else{
+        tree.remove(el);
+        tree.printbash();
+    }
+    
+}
+
+void countRecursive(const HashTable<string, string>& baza, const string& manager, HashTable<string, int>& bazacount) {
+    int count = 0;
+    for (int i = 0; i < baza.capacity; i++) {
+        if (!baza.table[i].key.empty() && !baza.table[i].isDeleted && baza.table[i].value == manager) {
+            count++;
+            countRecursive(baza, baza.table[i].key, bazacount);
+        }
+    }
+    bazacount.plusWorker(manager);
+}
+
+void task6(){
+    cout << "Пар:";
+    int capa;
+    cin >> capa;
+
+    HashTable<string,string> baza(capa*2);
+    HashTable <string, int> bazacount(capa*2);
+    for (int i = 0; i < capa; i++){
+        cout << "сотрудник - менеджер:";
+        string key;
+        string val;
+        cin >> key >> val;
+        baza.insert(key, val);
+        bazacount.insert(key, 0);
+        bazacount.insert(val, 0);
+    }
+    capa = capa * 2;
+
+    for (int i = 0; i < capa; i++){
+        if (!baza.table[i].key.empty() && !baza.table[i].isDeleted){
+            string manager = baza.table[i].value;
+            if (baza.table[i].key == baza.table[i].value){
+                bazacount.plusWorker(manager);
+            }
+            bazacount.plusWorker(manager);
+        }
+    }
+    bazacount.printbash();
 }
